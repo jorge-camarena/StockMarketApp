8a033162e15c4ce5c40728b1f4c9bd76cb52c7d4
jorge-camarena <jorgecam5813@gmail.com>
2024-05-09 17:10:17 -0700

    Set up AccountController and finishes models for the database entities

---
+++ StockManager.API/Models/Stock.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockManager.API.Models
+{
+    public class Stock
+    {
+        public Guid Id { get; set; }
+        public Guid PortfolioId { get; set; }
+        public string Symbol { get; set; } = string.Empty;
+        public float AmountInvested { get; set; }
+        public float SharesOwned { get; set; }
+        public DateTime AddedToPortfolioDateTime { get; set; }
+    }
+}
---
+++ StockManager.API/Models/Transactions.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockManager.API.Models
+{
+    public class Transactions
+    {
+        public Guid Id { get; set; }
+        public Guid PortfolioId { get; set; }
+        public string StockSymbol {get; set; } = string.Empty;
+        public float AmountInvested {get; set; }
+        public float SharesBought { get; set; }
+        public float ShareValueAtTransaction {get; set; }
+        public DateTime TimeOfTransactionDateTime { get; set; }
+    }
+}
